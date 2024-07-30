f4bc6308c41589a1265db722ddd9b1c7d1459064
Rokunuzzaman Rafi <rokunuzzamanrafi@gmail.com>
2024-07-30 14:19:43 +0600

    add Amazon project to Javascript repo

---
+++ javascript-amazon-project/jasmine-standalone-5.1.1/tests-simple/moneyTest.js
@@ -0,0 +1,34 @@
+import { formatCurrency } from "../../scripts/utils/money.js";
+
+console.log("test suite: formatCurrancy");
+
+console.log("converts cents into dollars");
+
+if (formatCurrency(2095) === "20.95") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("works with 0");
+if (formatCurrency(0) === "0.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("rounds up to the nearest cent");
+
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("rounds up to the nearest cent");
+
+if (formatCurrency(2000.4) === "20.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
